ab3236f5da54b10c42f376120d6d5168eedbfd44
Hrichik Mazumder <57860916+hrichiksite@users.noreply.github.com>
2021-11-25 16:29:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,11 @@ var foodObj;
 var gameState,readState;
 
 function preload(){
-sadDog=loadImage("Images/Dog.png");
-happyDog=loadImage("Images/happy dog.png");
-garden=loadImage("Images/Garden.png");
-washroom=loadImage("Images/Wash Room.png");
-bedroom=loadImage("Images/Bed Room.png");
+sadDog=loadImage("images/Dog.png");
+happyDog=loadImage("images/happy dog.png");
+garden=loadImage("images/Garden.png");
+washroom=loadImage("images/Wash Room.png");
+bedroom=loadImage("images/Bed Room.png");
 }
 
 function setup() {
